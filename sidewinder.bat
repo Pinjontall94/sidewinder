6ad9d9bc1f64c37a3a68e1f3fa9ed81f191499df
Sam Johnson <sambjohnson94@gmail.com>
2025-01-31 22:21:36 -0800

    simplify app entry scripts to call venv's python directly

--- sidewinder.bat
+++ sidewinder.bat
@@ -1,13 +1,4 @@
 @echo off
-
 REM Activate the python environment and install dependencies
-venv\Scripts\activate.bat
-python.exe -m pip install -f requirements.txt
-python.exe src\main.py
-cd public\
-python.exe -m http.server 8080
-
-REM Deactivate environment and exit to CMD
-cd ..
-venv\Scripts\deactivate.bat
-exit /B
+venv\Scripts\pip -m pip install -f requirements.txt
+venv\Scripts\python src\main.py
--- sidewinder.command
+++ sidewinder.command
@@ -1,4 +1,2 @@
-. venv/bin/activate
-python3 -m pip install -f requirements.txt 
-python3 src/main.py
-cd public && python -m http.server 8080
+venv/bin/pip install -f requirements.txt
+venv/bin/python3 src/main.py
